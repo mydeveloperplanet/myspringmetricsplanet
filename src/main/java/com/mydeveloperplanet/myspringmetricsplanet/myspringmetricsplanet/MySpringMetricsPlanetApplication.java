84e4ec36503662d90c7033f1b8c130b85e044fb9
mydeveloperplanet <gunter@mydeveloperplanet.com>
2021-02-17 20:15:16 +0100

    Initial commit

---
+++ src/main/java/com/mydeveloperplanet/myspringmetricsplanet/myspringmetricsplanet/MySpringMetricsPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myspringmetricsplanet.myspringmetricsplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringMetricsPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringMetricsPlanetApplication.class, args);
+	}
+
+}
